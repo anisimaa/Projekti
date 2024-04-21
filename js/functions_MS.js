0cf2e29909b0f937984a9732525c3ee8e3800595
miiasandelin <n3sami00@students.oamk.fi>
2024-04-21 18:28:57 +0300

    Raahausfunktiota jatkettu javascriptissä -MS

--- js/functions_MS.js
+++ js/functions_MS.js
@@ -1,6 +1,9 @@
 let listContainer
 let draggableItem
 
+let pointerSartX
+let pointerStartY
+
 function setup() {
     listContainer = document.querySelector('.js-list')
 
@@ -14,6 +17,14 @@ function setup() {
 function dragStart(e) {
     console.log('Drag start')
 
+    if(e.target.classList.contains('js-drag-handle')) {
+        draggableItem = e.target.closest('.js-item')
+    }
+
+    if (!draggableItem) return
+
+    initDraggableItem()
+
     document.addEventListener('mousemove', drag)
 }
 
@@ -25,6 +36,26 @@ function dragEnd() {
     console.log('Drag end')
 
     document.removeEventListener('mousemove', drag)
+}
+
+function initDraggableItem() {
+    draggableItem.classList.remove('is-idle')
+    draggableItem.classList.add('is-draggable')
+}
+
+function dragStart(e) {
+    if (e.target.classList.contains('js-drag-handle')) {
+        draggableItem = e.target.closest('.js-item')
+    }
+
+    if (!draggableItem) return
+
+    pointerSartX = e.clientX
+    pointerStartY =e.clientY
+
+    initDraggableItem()
+
+    document.addEventListener('mousemove', drag)
 }
 
 setup()
