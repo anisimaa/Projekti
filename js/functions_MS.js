8836aa2e9ab6c86a3dcaab94b0c0bc4186f25fd3
miiasandelin <n3sami00@students.oamk.fi>
2024-04-27 17:45:29 +0300

    Raahattavat elementit talletettu funktioon -MS

--- js/functions_MS.js
+++ js/functions_MS.js
@@ -1,9 +1,24 @@
 let listContainer
 let draggableItem
+let items = []
 
 let pointerSartX
 let pointerStartY
 
+//talletetaan kaikki elementit yhteen funktioon
+function getAllItems() {
+    if (!items?.length) {
+        items = Array.from(listContainer.querySelectorAll('.js-item'))
+    }
+    return items
+}
+
+//erotellaan elementeistä vain ne, joita voi raahata
+function getIdleItems() {
+    return getAllItems().filter((item) => item.classList.contains('is-idle'))
+}
+
+//raahaus
 function setup() {
     listContainer = document.querySelector('.js-list')
 
@@ -14,6 +29,7 @@ function setup() {
     document.addEventListener('mouseup', dragEnd)
 }
 
+//raahauksen aloitus
 function dragStart(e) {
     if (e.target.classList.contains('js-drag-handle')) {
         draggableItem = e.target.closest('.js-item')
@@ -25,10 +41,12 @@ function dragStart(e) {
     pointerStartY =e.clientY
 
     initDraggableItem()
+    initItemState()
 
     document.addEventListener('mousemove', drag)
 }
 
+//mitä voi raahata
 function drag(e) {
     console.log('Dragging')
     if(!draggableItem) return
@@ -42,6 +60,7 @@ function drag(e) {
     draggableItem.style.transform = `translate(${pointerOffsetX}px, ${pointerOffsetY}px)`
 }
 
+//palauttaa raahattavan elementin paikoilleen
 function unsetDraggableItem() {
     draggableItem.style = null
     draggableItem.classList.remove('is-draggable')
@@ -49,6 +68,7 @@ function unsetDraggableItem() {
     draggableItem = null
 }
 
+//raahauksen lopetus
 function dragEnd() {
     console.log('Drag end')
     if(!draggableItem) return
@@ -66,6 +86,18 @@ function cleanup() {
 function initDraggableItem() {
     draggableItem.classList.remove('is-idle')
     draggableItem.classList.add('is-draggable')
+}
+
+function initItemState() {
+    getIdleItems().forEach((item, i) => {
+        if (getAllItems().indexOf(draggableItem) > i) {
+            item.dataset.isAbove = ''
+        }
+    })
+}
+
+function getIdleItems() {
+    return getAllItems().filter((item) => item.classList.contains('is-idle'))
 }
 
 setup()
