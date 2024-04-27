b4fce42e8ce6128f64e5cf1caae33ed756436f2b
katasiimes <o2sika01@students.oamk.fi>
2024-04-27 19:02:11 +0300

    jotain yritin mutta en onnistunu -ks

--- js/functions_KS.js
+++ js/functions_KS.js
@@ -1,4 +1,26 @@
 function showMessage() {
-    document.getElementById('message').innerText = 'Oikein!';
-  }
+  document.getElementById('message').innerText = 'Oikein!';
+}
+
+let X = 300;
+let Y = 400;
+
+
+window.addEventListener('load', (event) => {
+  let img = document.querySelector('img');
+  let invisiblebutton = document.getElementById('invisiblebutton');
   
+  invisiblebutton.style.left = minimiX + 'px';
+  invisiblebutton.style.top = minimiY + 'px';
+  invisiblebutton.style.width = (maksimiX - minimiX) + 'px';
+  invisiblebutton.style.height = (maksimiY - minimiY) + 'px';
+
+
+  img.addEventListener("click", function(event) {
+    alert(`(${event.offsetX},${event.offsetY})`);
+  });
+});
+
+if (X > minimiX && X < maksimiX && Y > minimiY && Y < maksimiY) {
+  // Do something
+}
